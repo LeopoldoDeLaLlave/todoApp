e5cfbba901a9dc7c8902f039e1992416d7bf1dd2
LeopoldoDeLaLlave <58303142+LeopoldoDeLaLlave@users.noreply.github.com>
2020-06-11 18:39:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var todoController  =require('./controllers/todoController.js');
+
+var app = express();
+
+//set up template engines
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./public'));
+
+//fire controllers
+
+todoController(app);
+
+//listen to port
+app.listen(3000);
+console.log("port 3000");
